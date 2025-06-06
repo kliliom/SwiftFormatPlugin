7dcd8022c9582c057d4322b3acaf1a8e97715676
Kristof Liliom <177391622+kliliom@users.noreply.github.com>
2025-06-06 18:53:52 +0200

    feat: update to version 0.56.2

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "swiftformatplugin-binary",
-            url: "https://github.com/nicklockwood/SwiftFormat/releases/download/0.56.1/swiftformat.artifactbundle.zip",
-            checksum: "f8019853bf29fb9ecfbf53b5d26100e1c71f7b2fac4681176b13ae1674088f87"
+            url: "https://github.com/nicklockwood/SwiftFormat/releases/download/0.56.2/swiftformat.artifactbundle.zip",
+            checksum: "f23cdf64fa80f278f6fd1358c27234c36e66e219c62fb1f1e3e09bf89bbbaf4d"
         ),
         .plugin(
             name: "SwiftFormatPlugin",
